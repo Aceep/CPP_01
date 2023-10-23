ef61ca8def4d5edb2c797b393787df78c9d469a7
Alycia GAUTIER <alycgaut@made-f0Br6s6.clusters.42paris.fr>
2023-10-23 16:59:26 +0200

    cpp01

---
+++ ex01/include/Zombie.hpp
@@ -0,0 +1,19 @@
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+# include <iostream>
+# include <string>
+
+class Zombie
+{
+    private :
+        std::string _name;
+
+    public :
+        Zombie();
+        ~Zombie();
+        Zombie* zombieHorde( int N, std::string name );
+        void setName(std::string name);
+};
+
+#endif
---
+++ ex01/srcs/Zombie.cpp
@@ -0,0 +1,14 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie()
+{
+}
+
+Zombie::~Zombie()
+{
+}
+
+void Zombie::setName(std::string name)
+{
+    this->_name = name;
+}
